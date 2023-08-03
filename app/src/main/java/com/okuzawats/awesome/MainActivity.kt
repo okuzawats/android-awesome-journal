761e648b19d45ebccf7c218c73a9e0a03e46503c
okuzawats <okuzawats@gmail.com>
2023-08-04 05:30:05 +0900

    FABを表示

--- app/src/main/java/com/okuzawats/awesome/MainActivity.kt
+++ app/src/main/java/com/okuzawats/awesome/MainActivity.kt
@@ -3,26 +3,62 @@ package com.okuzawats.awesome
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.okuzawats.awesome.ui.theme.AwesomeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AwesomeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            MaterialTheme {
+                Box(
+                    contentAlignment = Alignment.BottomEnd,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .padding(
+                                bottom = 16.dp,
+                                end = 16.dp,
+                            )
+                    ) {
+                        FloatingActionButton(
+                            onClick = { /*TODO*/ },
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            shape = RoundedCornerShape(16.dp),
+                            modifier = Modifier
+                                .width(56.dp)
+                                .height(56.dp)
+                        ) {
+                            Icon(
+                                painter = painterResource(
+                                    id = R.drawable.baseline_add_24,
+                                ),
+                                contentDescription = "add",
+                                modifier = Modifier
+                                    .width(24.dp)
+                                    .height(24.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
