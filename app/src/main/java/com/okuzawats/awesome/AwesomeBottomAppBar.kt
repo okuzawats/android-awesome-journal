4c1da292bd48c8b0e86a2aacb940851e3388037c
okuzawats <okuzawats@gmail.com>
2023-08-05 15:57:52 +0900

    BottomAppBarをファイルに切り出し

---
+++ app/src/main/java/com/okuzawats/awesome/AwesomeBottomAppBar.kt
@@ -0,0 +1,54 @@
+package com.okuzawats.awesome
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+/**
+ * アプリ内で使用するBottomAppBar
+ *
+ * @param onFabClicked FABタップ時に発火するLambda
+ */
+@Composable
+fun AwesomeBottomAppBar(
+    modifier: Modifier = Modifier,
+    onFabClicked: () -> Unit,
+) {
+    // FIXME BottomAppBarの引数にActionとFABを渡すように修正する。
+    //  現在、上記のコードがビルドエラーとってしまうため、自力で配置する。
+    BottomAppBar {
+        Row(
+            modifier = modifier
+                .padding(top = 12.dp, end = 16.dp, bottom = 12.dp),
+        ) {
+            // FABをRowの末尾に配置するためのSpacer
+            Spacer(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
+            )
+            FloatingActionButton(
+                onClick = onFabClicked,
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_add_24),
+                    // TODO contentDescriptionの設定
+                    contentDescription = "add",
+                    modifier = Modifier
+                        .width(24.dp)
+                        .height(24.dp),
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/okuzawats/awesome/MainActivity.kt
+++ app/src/main/java/com/okuzawats/awesome/MainActivity.kt
@@ -3,23 +3,13 @@ package com.okuzawats.awesome
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -29,35 +19,13 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 Scaffold(
                     bottomBar = {
-                        // FIXME BottomAppBarの引数にActionとFABを渡すように修正する。
-                        //  現在、上記のコードがビルドエラーとってしまうため、自力で配置する。
-                        BottomAppBar {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 12.dp, end = 16.dp, bottom = 12.dp),
-                            ) {
-                                Spacer(
-                                    modifier = Modifier
-                                        .weight(1f)
-                                        .fillMaxHeight(),
-                                )
-                                FloatingActionButton(
-                                    onClick = {
-                                        /*TODO*/
-                                    },
-                                ) {
-                                    Icon(
-                                        painter = painterResource(R.drawable.baseline_add_24),
-                                        contentDescription = "add", // TODO contentDescriptionの設定
-                                        modifier = Modifier
-                                            .width(24.dp)
-                                            .height(24.dp),
-                                    )
-
-                                }
-                            }
-                        }
+                        AwesomeBottomAppBar(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            onFabClicked = {
+                                // TODO
+                            },
+                        )
                     },
                 ) { padding ->
                     Text(
