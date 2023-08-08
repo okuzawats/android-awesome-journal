73927ed309654de60bcf99ec64d019ed8949c7ac
okuzawats <okuzawats@gmail.com>
2023-08-08 22:11:07 +0900

    DefaultBulletRepositoryを実装

--- app/src/main/java/com/okuzawats/awesome/repository/DefaultBulletRepository.kt
+++ app/src/main/java/com/okuzawats/awesome/repository/DefaultBulletRepository.kt
@@ -1,6 +1,17 @@
 package com.okuzawats.awesome.repository
 
+import com.okuzawats.awesome.domain.bullet.Bullet
 import com.okuzawats.awesome.domain.bullet.BulletRepository
+import java.util.UUID
 
 class DefaultBulletRepository : BulletRepository {
+    override suspend fun getBullets(): List<Bullet> {
+        return (0 ..< 100).map {
+            Bullet(
+                id = UUID.randomUUID(),
+                text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                done = false,
+            )
+        }
+    }
 }
