188890b394d6d3cf5d61a9fbc70932f4c7e24813
okuzawats <okuzawats@gmail.com>
2023-08-09 08:46:14 +0900

    UiEvent / UiStateを作成

---
+++ app/src/main/java/com/okuzawats/awesome/presenter/UiEvent.kt
@@ -0,0 +1,8 @@
+package com.okuzawats.awesome.presenter
+
+import com.slack.circuit.runtime.CircuitUiEvent
+
+/**
+ * UIで発生したイベントを表すinterface
+ */
+interface UiEvent  : CircuitUiEvent
---
+++ app/src/main/java/com/okuzawats/awesome/presenter/UiState.kt
@@ -0,0 +1,8 @@
+package com.okuzawats.awesome.presenter
+
+import com.slack.circuit.runtime.CircuitUiState
+
+/**
+ * UIの状態を表すinterface
+ */
+interface UiState : CircuitUiState
