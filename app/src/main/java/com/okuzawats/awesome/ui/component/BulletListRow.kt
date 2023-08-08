6ee973619b759d46f42f9eb9e4f30b07c8ddbe6d
okuzawats <okuzawats@gmail.com>
2023-08-09 08:57:56 +0900

    Bulletの表示を修正

--- app/src/main/java/com/okuzawats/awesome/ui/component/BulletListRow.kt
+++ app/src/main/java/com/okuzawats/awesome/ui/component/BulletListRow.kt
@@ -1,20 +1,20 @@
 package com.okuzawats.awesome.ui.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -35,15 +35,10 @@ fun BulletListRow(
     bulletText: String,
     onBulletClick: () -> Unit,
 ) {
-    Box(
-        modifier = modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp),
-    ) {
+    Column {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = modifier
-                .clip(shape = RoundedCornerShape(4.dp))
-                .background(color = Color.White)
                 .clickable(onClick = onBulletClick),
         ) {
             Spacer(
@@ -74,6 +69,12 @@ fun BulletListRow(
                     .width(24.dp),
             )
         }
+        Divider(
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp),
+        )
     }
 }
 
