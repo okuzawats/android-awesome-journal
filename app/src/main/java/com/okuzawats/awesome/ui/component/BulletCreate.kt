b9fe660d12d908417c4164cfe2cf53ac12bb2f4d
okuzawats <okuzawats@gmail.com>
2023-08-10 23:26:07 +0900

    BulletCreateを作成

---
+++ app/src/main/java/com/okuzawats/awesome/ui/component/BulletCreate.kt
@@ -0,0 +1,15 @@
+package com.okuzawats.awesome.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * Bullet作成画面
+ */
+@Composable
+fun BulletCreate() {
+    Column {
+        Text("Create Bullet Here")
+    }
+}
