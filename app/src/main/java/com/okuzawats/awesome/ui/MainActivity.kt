265cf33d329ddbb43be0f64364021c3a858f1815
okuzawats <okuzawats@gmail.com>
2023-08-12 15:58:44 +0900

    不要なアノテーションを削除

--- app/src/main/java/com/okuzawats/awesome/ui/MainActivity.kt
+++ app/src/main/java/com/okuzawats/awesome/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -13,7 +12,6 @@ import com.okuzawats.awesome.ui.component.AwesomeBottomAppBar
 import com.okuzawats.awesome.ui.screen.BulletList
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
