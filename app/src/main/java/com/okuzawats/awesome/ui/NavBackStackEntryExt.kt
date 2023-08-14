8432d61db1d0e713e226cbc95c55db65bfdeafa7
okuzawats <okuzawats@gmail.com>
2023-08-14 23:17:24 +0900

    KDocの追加

--- app/src/main/java/com/okuzawats/awesome/ui/NavBackStackEntryExt.kt
+++ app/src/main/java/com/okuzawats/awesome/ui/NavBackStackEntryExt.kt
@@ -3,4 +3,9 @@ package com.okuzawats.awesome.ui
 import android.os.Bundle
 import androidx.navigation.NavBackStackEntry
 
+/**
+ * NavBackStackEntryからBundleを取り出す。
+ *
+ * @throws IllegalArgumentException Bundleがnullだった場合にthrowされる。
+ */
 fun NavBackStackEntry.requireArgument(): Bundle = requireNotNull(arguments)
