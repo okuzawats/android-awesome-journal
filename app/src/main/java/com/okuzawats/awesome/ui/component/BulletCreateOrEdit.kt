2ef31363230dfce6e81e7f1bab74faf99474ebef
okuzawats <okuzawats@gmail.com>
2023-08-14 23:15:15 +0900

    Composableのリネーム

--- app/src/main/java/com/okuzawats/awesome/ui/component/BulletCreateOrEdit.kt
+++ app/src/main/java/com/okuzawats/awesome/ui/component/BulletCreateOrEdit.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
  * @param bulletId 編集するBulletのID。新規作成の時はnull。
  */
 @Composable
-fun BulletCreate(
+fun BulletCreateOrEdit(
   bulletId: String?,
 ) {
   Column {
