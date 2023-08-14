78dac24e7635598120fe55387a48b47d21de36ee
okuzawats <okuzawats@gmail.com>
2023-08-14 23:17:53 +0900

    KDocの追加

--- app/src/main/java/com/okuzawats/awesome/AwesomeApp.kt
+++ app/src/main/java/com/okuzawats/awesome/AwesomeApp.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import com.okuzawats.awesome.module.bulletListModule
 import org.koin.core.context.startKoin
 
+/**
+ * カスタムアプリケーションクラス
+ */
 class AwesomeApp : Application() {
   override fun onCreate() {
     super.onCreate()
