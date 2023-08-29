b2d2ed552697a27b233a2fc9028e9e05eefd62db
okuzawats <okuzawats@gmail.com>
2023-08-29 13:28:53 +0900

    TopAppBarに保存ボタンを表示

--- app/src/main/java/com/okuzawats/awesome/ui/component/BulletCreateOrEdit.kt
+++ app/src/main/java/com/okuzawats/awesome/ui/component/BulletCreateOrEdit.kt
@@ -1,10 +1,21 @@
 package com.okuzawats.awesome.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.okuzawats.awesome.R
 
 /**
@@ -13,6 +24,7 @@ import com.okuzawats.awesome.R
  * @param bulletId 編集するBulletのID。新規作成の時はnull。
  * @param onDisposed Viewが破棄された時のコールバック
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BulletCreateOrEdit(
   bulletId: String?,
@@ -23,10 +35,23 @@ fun BulletCreateOrEdit(
   }
 
   Column {
-    if (bulletId != null) {
-      Text(bulletId)
-    } else {
-      Text(stringResource(id = R.string.create_new))
-    }
+    TopAppBar(
+      title = {
+        if (bulletId != null) {
+          Text(bulletId)
+        } else {
+          Text(stringResource(id = R.string.create_new))
+        }
+      },
+      actions = {
+        Image(
+          painter = painterResource(id = R.drawable.baseline_save_24),
+          colorFilter = ColorFilter.tint(Color.Black),
+          contentDescription = "save", // TODO
+          modifier = Modifier.padding(8.dp),
+        )
+      },
+    )
+    Text("test")
   }
 }
