689fde7232e41fd8a55688dfc026c9ba3dcd75a5
okuzawats <okuzawats@gmail.com>
2023-08-29 23:36:07 +0900

    戻るボタンを実装

--- app/src/main/java/com/okuzawats/awesome/presenter/bulletedit/BulletEditViewModel.kt
+++ app/src/main/java/com/okuzawats/awesome/presenter/bulletedit/BulletEditViewModel.kt
@@ -1,15 +1,17 @@
 package com.okuzawats.awesome.presenter.bulletedit
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.okuzawats.awesome.presenter.bulletedit.navigator.BulletEditNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BulletEditViewModel @Inject constructor(
   private val navigator: BulletEditNavigator,
 ) : ViewModel() {
-  fun onBackClicked() {}
+  fun onBackClicked() = viewModelScope.launch { navigator.popEdit() }
 
   fun onSaveClicked() {}
 }
