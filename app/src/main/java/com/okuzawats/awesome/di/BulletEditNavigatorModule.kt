52bea0bf055195123bcef8c8d1546d3ca781387d
okuzawats <okuzawats@gmail.com>
2023-08-29 23:30:47 +0900

    NavigatorModuleをBindsに置き換え

--- app/src/main/java/com/okuzawats/awesome/di/BulletEditNavigatorModule.kt
+++ app/src/main/java/com/okuzawats/awesome/di/BulletEditNavigatorModule.kt
@@ -2,14 +2,14 @@ package com.okuzawats.awesome.di
 
 import com.okuzawats.awesome.presenter.bulletedit.navigator.BulletEditNavigator
 import com.okuzawats.awesome.ui.navigator.MainNavigator
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-class BulletEditNavigatorModule {
-  @Provides
-  fun provideBulletEditNavigator(mainNavigator: MainNavigator): BulletEditNavigator = mainNavigator
+abstract class BulletEditNavigatorModule {
+  @Binds
+  abstract fun bindBulletEditNavigator(mainNavigator: MainNavigator): BulletEditNavigator
 }
--- app/src/main/java/com/okuzawats/awesome/di/BulletListNavigatorModule.kt
+++ app/src/main/java/com/okuzawats/awesome/di/BulletListNavigatorModule.kt
@@ -2,14 +2,14 @@ package com.okuzawats.awesome.di
 
 import com.okuzawats.awesome.presenter.bulletlist.navigator.BulletListNavigator
 import com.okuzawats.awesome.ui.navigator.MainNavigator
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-class BulletListNavigatorModule {
-  @Provides
-  fun provideBulletListNavigator(mainNavigator: MainNavigator): BulletListNavigator = mainNavigator
+abstract class BulletListNavigatorModule {
+  @Binds
+  abstract fun provideBulletListNavigator(mainNavigator: MainNavigator): BulletListNavigator
 }
