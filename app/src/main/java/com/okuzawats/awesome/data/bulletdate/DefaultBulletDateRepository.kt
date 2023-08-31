89323e502cb05d21d59e2118d8fce6be722b060c
okuzawats <okuzawats@gmail.com>
2023-08-31 22:19:29 +0900

    テスト用の仮コード

--- app/src/main/java/com/okuzawats/awesome/data/bulletdate/DefaultBulletDateRepository.kt
+++ app/src/main/java/com/okuzawats/awesome/data/bulletdate/DefaultBulletDateRepository.kt
@@ -2,13 +2,46 @@ package com.okuzawats.awesome.data.bulletdate
 
 import com.okuzawats.awesome.domain.bulletdate.BulletDate
 import com.okuzawats.awesome.domain.bulletdate.BulletDateRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.Date
+import java.util.UUID
 import javax.inject.Inject
 
 class DefaultBulletDateRepository @Inject constructor(
   private val bulletDataDao: BulletDateDao,
 ) : BulletDateRepository {
   override suspend fun getAll(): List<BulletDate> {
-    TODO("Not yet implemented")
+//    return bulletDataDao
+//      .getAll()
+//      .map {
+//        // TODO
+//        BulletDate(
+//          UUID.randomUUID(),
+//          Date(),
+//        )
+//      }
+    return withContext(Dispatchers.IO) {
+      bulletDataDao
+        .save(
+          BulletDateEntity("", "")
+        )
+      bulletDataDao
+        .getAll()
+        .map {
+          // TODO
+          BulletDate(
+            UUID.randomUUID(),
+            Date(),
+          )
+        }
+//      listOf(
+//        BulletDate(
+//          UUID.randomUUID(),
+//          Date(),
+//        )
+//      )
+    }
   }
 
   override suspend fun getCurrentDate(): BulletDate {
---
+++ app/src/test/java/com/okuzawats/awesome/data/bulletdate/DefaultBulletDateRepositoryTest.kt
@@ -0,0 +1,60 @@
+package com.okuzawats.awesome.data.bulletdate
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.okuzawats.awesome.data.AwesomeDatabase
+import com.okuzawats.awesome.domain.bulletdate.BulletDateRepository
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import javax.inject.Inject
+
+@HiltAndroidTest
+@Config(
+  application = HiltTestApplication::class,
+  manifest = Config.NONE,
+)
+@RunWith(AndroidJUnit4::class)
+class DefaultBulletDateRepositoryTest {
+
+  @get:Rule
+  var hiltRule = HiltAndroidRule(this)
+
+  @Inject lateinit var sut: BulletDateRepository
+
+  @Inject lateinit var bulletListDao: BulletDateDao
+
+  @Inject lateinit var database: AwesomeDatabase
+
+  @Before
+  fun setUp() {
+    hiltRule.inject()
+  }
+
+  @After
+  fun tearDown() {
+    database.close()
+  }
+
+  @Test
+  fun getAll() = runTest {
+    val actual = sut.getAll()
+
+    assertThat(actual).hasSize(1)
+  }
+
+  @Test
+  fun getCurrentDate() {
+  }
+
+  @Test
+  fun createCurrentDate() {
+  }
+}
