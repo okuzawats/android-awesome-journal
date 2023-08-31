4fbb11f5254fe8b60dc491f3743ca2a07345ff4a
okuzawats <okuzawats@gmail.com>
2023-08-31 22:15:58 +0900

    テスト用のExecuterを指定

--- app/src/test/java/com/okuzawats/awesome/FakeAwesomeDatabaseBuilder.kt
+++ app/src/test/java/com/okuzawats/awesome/FakeAwesomeDatabaseBuilder.kt
@@ -1,18 +1,21 @@
 package com.okuzawats.awesome
 
-import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.okuzawats.awesome.data.AwesomeDatabase
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.test.StandardTestDispatcher
 import javax.inject.Inject
 
 class FakeAwesomeDatabaseBuilder @Inject constructor() {
   fun build(): AwesomeDatabase {
     return Room
       .inMemoryDatabaseBuilder(
-        context = ApplicationProvider.getApplicationContext<Context>(),
+        context = ApplicationProvider.getApplicationContext(),
         klass = AwesomeDatabase::class.java,
       )
+      .setTransactionExecutor(StandardTestDispatcher().asExecutor())
+      .setQueryExecutor(StandardTestDispatcher().asExecutor())
       .build()
   }
 }
