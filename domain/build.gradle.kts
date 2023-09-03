e1440166a1b547337a3f8f1e4e8638c9af6d616c
okuzawats <okuzawats@gmail.com>
2023-09-03 14:41:47 +0900

    build.gradleにコメントを追加

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -40,18 +40,30 @@ android {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+  // Kotlin Coroutines（特にFlow）
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+  // 依存性注入
   implementation("com.google.dagger:hilt-android:2.47")
   kapt("com.google.dagger:hilt-compiler:2.47")
-
+  // 単体テスト用のフレームワーク
   testImplementation("junit:junit:4.13.2")
+  // JUnit 4とHiltを組み合わせてテストランナーを動かすために使用
   testImplementation("androidx.test.ext:junit-ktx:1.1.5")
+  // テストコードでCoroutinesを動かすために使用
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+  // テストコードでFluent Assertionを書くために使用
   testImplementation("com.google.truth:truth:1.1.4")
+  // テストダブルの作成に使用
   testImplementation("io.mockk:mockk:1.13.1")
+  // Flowのテストのために使用
   testImplementation("app.cash.turbine:turbine:1.0.0")
+  // テストコードでAndroid特有のコードを動かすために使用
   testImplementation("org.robolectric:robolectric:4.10.3")
+  // テストコードでHiltを用いた依存性注入を行うために使用
   testImplementation("com.google.dagger:hilt-android-testing:2.47")
   kaptTest("com.google.dagger:hilt-android-compiler:2.47")
+}
+
+kapt {
+  correctErrorTypes = true // hilt
 }
