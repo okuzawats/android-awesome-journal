1362dd0abb4ed228c36b1da6841234e376e93155
okuzawats <okuzawats@gmail.com>
2023-09-06 21:57:16 +0900

    @Provideアノテーションを用いた記法を用いるように修正

--- app/src/main/java/com/okuzawats/awesome/di/domain/UnDoneToDoUseCaseModule.kt
+++ app/src/main/java/com/okuzawats/awesome/di/domain/UnDoneToDoUseCaseModule.kt
@@ -2,14 +2,25 @@ package com.okuzawats.awesome.di.domain
 
 import com.okuzawats.awesome.domain.undone.DefaultUnDoneToDoUseCase
 import com.okuzawats.awesome.domain.undone.UnDoneToDoUseCase
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+//@Module
+//@InstallIn(SingletonComponent::class)
+//abstract class UnDoneToDoUseCaseModule {
+//  @Binds
+//  abstract fun bindUnDoneToDoUseCase(impl: DefaultUnDoneToDoUseCase): UnDoneToDoUseCase
+//}
+
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class UnDoneToDoUseCaseModule {
-  @Binds
-  abstract fun bindUnDoneToDoUseCase(impl: DefaultUnDoneToDoUseCase): UnDoneToDoUseCase
+class UnDoneToDoUseCaseModule {
+  @Provides
+  fun bindUnDoneToDoUseCase(
+    impl: DefaultUnDoneToDoUseCase,
+  ): UnDoneToDoUseCase {
+    return impl
+  }
 }
