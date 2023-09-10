1807742f2a332c6b8fcb5b001d879f09fb088456
okuzawats <okuzawats@gmail.com>
2023-09-10 23:18:20 +0900

    kspを有効化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
   id("org.jetbrains.kotlin.kapt")
+  id("com.google.devtools.ksp")
   id("com.google.dagger.hilt.android")
 }
 
