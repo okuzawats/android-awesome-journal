c6c0dd63a1fe643abaf29524b9e2d2efe8a4297a
okuzawats <okuzawats@gmail.com>
2023-09-23 16:04:37 +0900

    Ignoreされるテストの追加

--- app/src/test/java/com/okuzawats/awesome/presenter/bulletlist/BulletListViewModelTest.kt
+++ app/src/test/java/com/okuzawats/awesome/presenter/bulletlist/BulletListViewModelTest.kt
@@ -3,14 +3,23 @@ package com.okuzawats.awesome.presenter.bulletlist
 import com.okuzawats.awesome.domain.bullet.Bullet
 import com.okuzawats.awesome.domain.bullet.BulletRepository
 import com.okuzawats.awesome.domain.bullet.BulletType
+import com.okuzawats.awesome.domain.bulletdate.BulletDate
 import com.okuzawats.awesome.presenter.bulletlist.navigator.BulletListNavigator
 import com.okuzawats.awesome.presenter.bulletlist.reducer.BulletListReducer
+import com.okuzawats.awesome.presenter.bulletlist.state.BulletList
+import com.okuzawats.awesome.presenter.bulletlist.state.BulletListState
 import com.okuzawats.awesome.rule.MainDispatcherRule
+import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
+import java.text.SimpleDateFormat
 
 class BulletListViewModelTest {
 
@@ -23,6 +32,10 @@ class BulletListViewModelTest {
   private lateinit var reducer: BulletListReducer
   private lateinit var navigator: BulletListNavigator
   private lateinit var dateProvider: DateProvider
+
+  companion object {
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+  }
 
   @Before
   fun setUp() {
@@ -40,7 +53,13 @@ class BulletListViewModelTest {
   }
 
   @Test
-  fun present() {
+  @Ignore("Stateのテストが書けるようになるまで無効化")
+  fun present() = runTest {
+    val today = dateFormat.parse("2023-09-23")!!
+    every { dateProvider.today() } returns today
+    coEvery { bulletRepository.getBulletsAt(BulletDate(any(), today)) }
+    every { reducer.state } returns MutableStateFlow<BulletListState>(
+      BulletList(date = today, bullets = emptyList()))
   }
 
   @Test
