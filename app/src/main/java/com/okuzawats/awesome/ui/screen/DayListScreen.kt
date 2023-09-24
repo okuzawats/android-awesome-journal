3c593856f12218cc62fef04e191c8c16914cd907
okuzawats <okuzawats@gmail.com>
2023-09-24 16:16:38 +0900

    KDocを追加

--- app/src/main/java/com/okuzawats/awesome/ui/screen/DayListScreen.kt
+++ app/src/main/java/com/okuzawats/awesome/ui/screen/DayListScreen.kt
@@ -10,6 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.okuzawats.awesome.ui.component.TopAppBar
 
+/**
+ * DrawerMenuから開かれる日付一覧スクリーン
+ *
+ * FIXME: Drawerを閉じた時にこの画面の一部が消えずに残ってしまうため、[isOpened]で表示を制御している。
+ * @param isOpened 自分自身が開かれている時はtrue
+ * @param onBackClicked 戻るボタンがクリックされた時のコールバック
+ */
 @Composable
 fun DayListScreen(
   isOpened: Boolean,
